c1bd86fa13d7321fa7c0ada2a1404032e63fba01
Michal Trubini <mchl.trubini@gmail.com>
2024-01-31 15:07:12 +0100

    wip

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -16,19 +16,19 @@ class Job extends Model
 
     public function scopeFilter(EloquentBuilder|Builder $query, array $filters) : EloquentBuilder|Builder
     {
-        return $query->when(request('search'), function($query) {
-            $query->where(function($query){
-                $query->where('title', 'like', '%'.request('search').'%')
-                ->orWhere('description', 'like', '%'.request('search').'%');
+        return $query->when($filters['search'] ?? null, function ($query, $search) {
+            $query->where(function ($query) use ($search) {
+                $query->where('title', 'like', '%' . $search . '%')
+                    ->orWhere('description', 'like', '%' . $search . '%');
             });
-        })->when(request('min_salary'), function($query) {
-            $query->where('salary', '>=', request('min_salary'));
-        })->when(request('max_salary'), function($query) {
-            $query->where('salary', '<=', request('max_salary'));
-        })->when(request('experience'), function($query){
-            $query->where('experience', request('experience'));
-        })->when(request('category'), function($query){
-            $query->where('category', request('category'));
+        })->when($filters['min_salary'] ?? null, function ($query, $minSalary) {
+            $query->where('salary', '>=', $minSalary);
+        })->when($filters['max_salary'] ?? null, function ($query, $maxSalary) {
+            $query->where('salary', '<=', $maxSalary);
+        })->when($filters['experience'] ?? null, function ($query, $experience) {
+            $query->where('experience', $experience);
+        })->when($filters['category'] ?? null, function ($query, $category) {
+            $query->where('category', $category);
         });
     }
 }
